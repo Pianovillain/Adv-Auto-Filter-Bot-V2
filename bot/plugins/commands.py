4cb5ddf9d8c74f41f968b280684af6e5c3045d22
Pianovillain <86278468+Pianovillain@users.noreply.github.com>
2021-11-14 14:38:52 +0530

    hehe just my things

--- bot/plugins/commands.py
+++ bot/plugins/commands.py
@@ -35,7 +35,7 @@ async def start(bot, update):
                         [
                             InlineKeyboardButton
                                 (
-                                    'Developers', url="https://t.me/CrazyBotsz"
+                                    'Developers', url="https://t.me/plugincloudsarchive"
                                 )
                         ]
                     ]
@@ -47,10 +47,10 @@ async def start(bot, update):
         return
 
     buttons = [[
-        InlineKeyboardButton('Developers', url='https://t.me/CrazyBotsz'),
-        InlineKeyboardButton('Source Code 🧾', url ='https://github.com/CrazyBotsz/Adv-Auto-Filter-Bot-V2')
+        InlineKeyboardButton('Developers', url='https://t.me/remoguardbot'),
+        InlineKeyboardButton('Source Code 🧾', url ='https://www.theraleighregister.com/filter-bot-.html')
     ],[
-        InlineKeyboardButton('Support 🛠', url='https://t.me/CrazyBotszGrp')
+        InlineKeyboardButton('Support 🛠', url='https://t.me/remoguardbot')
     ],[
         InlineKeyboardButton('Help ⚙', callback_data="help")
     ]]
